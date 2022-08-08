817b0829a84c82f7183e67b9edced24b0dbc6c77
GenzyBassey <kextracol@gmail.com>
2022-08-08 06:57:06 +0100

    authentication system

---
+++ models/indexs.js
@@ -0,0 +1,10 @@
+//index.js file under models folder
+const User = require('./User')
+const PasswordReset = require('./PasswordReset')
+const UserVerification = require('./UserVerification')
+
+module.exports = {
+  User,
+  PasswordReset,
+  UserVerification
+}
